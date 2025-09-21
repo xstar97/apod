1239a27a1bf23b3971b3dc4b1f1d5d6c7b811286
Xstar97TheNoob <9399967+xstar97@users.noreply.github.com>
2025-09-21 15:35:29 -0400

    fix

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,16 +1,24 @@
 import React from "react";
+import { getCurrentDate } from "../Utils";
 import "./NavigationBar.css";
 
 const NavigationBar = ({ currentDate, onDateChange, onCalendarChange }) => {
+  const today = getCurrentDate();
+
   return (
     <div className="navigation-bar">
       <button onClick={() => onDateChange(-1)}>PREV</button>
+
       <input
         type="date"
         value={currentDate}
         onChange={(e) => onCalendarChange(e.target.value)}
+        max={today} // prevent selecting future dates
       />
-      <button onClick={() => onDateChange(1)}>NEXT</button>
+
+      {currentDate !== today && (
+        <button onClick={() => onDateChange(1)}>NEXT</button>
+      )}
     </div>
   );
 };
