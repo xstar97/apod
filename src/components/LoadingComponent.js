d32363218e256f85583e22d717d87574f7b2dde9
Xstar97TheNoob <9399967+xstar97@users.noreply.github.com>
2025-09-21 14:19:25 -0400

    update LoadingComponent and add css

--- src/components/LoadingComponent.js
+++ src/components/LoadingComponent.js
@@ -1,7 +1,38 @@
 import React from 'react';
+import './Loading.css'; // we'll define shimmer styles here
 
-const LoadingComponent = () => (
-  <div id="loading-view">Loading...</div>
-);
+const LoadingComponent = () => {
+  return (
+    <div id="image-container" style={{ margin: '20px' }}>
+      {/* Title skeleton */}
+      <div className="skeleton" style={{ width: '60%', height: '24px', marginBottom: '10px' }}></div>
+
+      {/* Date skeleton */}
+      <div className="skeleton" style={{ width: '30%', height: '16px', marginBottom: '10px' }}></div>
+
+      {/* Copyright skeleton */}
+      <div className="skeleton" style={{ width: '40%', height: '16px', marginBottom: '20px' }}></div>
+
+      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+        {/* Media skeleton */}
+        <div className="skeleton" style={{ width: '512px', height: '512px', margin: '10px', borderRadius: '8px' }}></div>
+
+        {/* Description skeleton */}
+        <div
+          style={{
+            width: '512px',
+            margin: '10px',
+            padding: '10px',
+            border: '1px solid #ccc',
+          }}
+        >
+          <div className="skeleton" style={{ width: '100%', height: '16px', marginBottom: '8px' }}></div>
+          <div className="skeleton" style={{ width: '90%', height: '16px', marginBottom: '8px' }}></div>
+          <div className="skeleton" style={{ width: '80%', height: '16px' }}></div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default LoadingComponent;
